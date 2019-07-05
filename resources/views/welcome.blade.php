3900e2ec8ebd992c1212e607f83c6afced84f6d7
reginadwi <rdwiapriliyanti@gmail.com>
2019-07-05 14:34:55 +0700

    tambah table kelas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Reginadwi
+                    ReginaDwiApriliyanti
                 </div>
 
                 <div class="links">
