4903f8a8a83ff6225638b2f58361bf51b6c14ede
reginadwi <rdwiapriliyanti@gmail.com>
2019-07-15 07:54:58 +0700

    mengubah register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,16 +63,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-    }
-        public function showRegistrationForm()
-        {
-            return view('errors.404');
-        }
+    //     return User::create([
+    //         'name' => $data['name'],
+    //         'email' => $data['email'],
+    //         'password' => Hash::make($data['password']),
+    //     ]);
+    // }
+    //     public function showRegistrationForm()
+    //     {
+    //         return view('errors.404');
+    //     }
     }
     
 
